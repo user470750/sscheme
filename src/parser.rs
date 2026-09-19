d018af2c10587c77de024e2c5a0368d33873c6eb
oleja <shabanovoleg04@gmail.com>
2026-09-19 18:50:22 +0300

    refactor(parser): use `chumsky` prelude
    Replace per-item `chumsky` imports with `chumsky::prelude`

--- src/parser.rs
+++ src/parser.rs
@@ -1,22 +1,16 @@
+use chumsky::input::ValueInput;
+use chumsky::prelude::*;
+
 use crate::lexer::Token;
 use crate::symbol::Symbol;
 use crate::value::Value;
-use chumsky::error::Rich;
-use chumsky::extra;
-use chumsky::input::ValueInput;
-use chumsky::primitive;
-use chumsky::recursive;
-use chumsky::select;
-use chumsky::span::SimpleSpan;
-use chumsky::IterParser;
-use chumsky::Parser;
 
 pub fn parser<'a, I>() -> impl Parser<'a, I, Value, extra::Err<Rich<'a, Token>>>
 where
     I: ValueInput<'a, Token = Token, Span = SimpleSpan>,
 {
-    let nil = primitive::just(Token::LParen)
-        .then(primitive::just(Token::RParen))
+    let nil = just(Token::LParen)
+        .then(just(Token::RParen))
         .map(|_| Value::Nil);
 
     let atom = select! {
@@ -24,8 +18,8 @@ where
         Token::Number(number) => Value::Number(number),
     };
 
-    recursive::recursive(|s_expression| {
-        let quoted = primitive::just(Token::Quote)
+    recursive(|s_expression| {
+        let quoted = just(Token::Quote)
             .then(s_expression.clone())
             .map(|(_, expr)| Value::List(vec![Value::Symbol(Symbol::from("quote")), expr]));
 
@@ -34,19 +28,13 @@ where
             .repeated()
             .collect()
             .map(|exps: Vec<Value>| Value::List(exps))
-            .delimited_by(
-                primitive::just(Token::LParen),
-                primitive::just(Token::RParen),
-            );
+            .delimited_by(just(Token::LParen), just(Token::RParen));
 
         let dot_notation = s_expression
             .clone()
-            .then_ignore(primitive::just(Token::Dot))
+            .then_ignore(just(Token::Dot))
             .then(s_expression)
-            .delimited_by(
-                primitive::just(Token::LParen),
-                primitive::just(Token::RParen),
-            )
+            .delimited_by(just(Token::LParen), just(Token::RParen))
             .map(|(car, cdr)| match cdr {
                 Value::Nil => {
                     if let Value::List(pair) = car {
